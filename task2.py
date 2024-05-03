bfb96309ae34455471230f64a29467dde5b8c53e
Sebastiaan <bas.terkeurs@gmail.com>
2024-05-02 19:07:30 -0700

    done :+1:

--- task2.py
+++ task2.py
@@ -8,28 +8,16 @@ displays instructions and how to play
 game()
 plays the games
 """
+##imports
 import random
 import os 
-import math
 import time
+##setup
 os.system('cls')
-def title():
-    print("----------------------------BLACK JAX---------------------------------")
-    print("--------------------------Press 'ENTER' to continue-------------------")
-    input()
-    os.system('cls')
-    print("----------------------------BLACK JAX---------------------------------")
-    time.sleep(1)
-    print("your goal is to get as close as you can to 21 without going over it...")
-    time.sleep(1)
-    print("you can hit (grab another card) or stand (let the dealer play)")
-    print("Dealer always cheques your pot and you pay a premium for betting...")
-    print("--------------------------Press 'ENTER' to continue-------------------")
-    input()
 
-def game():
-    total = 0
-    
+
+##shuffle animation
+def shuffle():    
     os.system('cls')
     time.sleep(1)
     print("The Dealer shuffles the deck")
@@ -44,45 +32,73 @@ def game():
     print("The Dealer shuffles the deck...")
     time.sleep(1)
     os.system('cls')
-##cards
-    print("\n\n         hit '1' or stand '2'")
-    choice = input()
-    if choice == 1:
-        card1 = random.randint(1,13)
-        if card1 == 1:
-            print("Ace")
-        if card1 == 2:
-            print("2")
-        if card1 == 3:
-            print("3")
-        if card1 == 4:
-            print("4")
-        if card1 == 5:
-            print("5")
-        if card1 == 6:
-            print("6")
-        if card1 == 7:
-            print("7")
-        if card1 == 8:
-            print("8")
-        if card1 == 9:
-            print("9")
-        if card1 == 10:
-            print("10")
-        if card1 == 11:
-            print("Jack")
-        if card1 == 12:
-            print("Queen")
-        if card1 == 13:
-            print("King")
-    print("                            ")
-    print("  __________ ")
-    print(f" |       {card1}  |               ")
-    print(" |          |               ")
-    print(" |          |               ")
-    print(f" |    {card1}      |               ")
-    print(" |          |               ")
-    print(f" |{card1}          |               ")
-    print(" |__________|                         ")
+##title
+def title():
+    print("----------------------------BLACK JAX---------------------------------")
+    print("--------------------------Press 'ENTER' to continue-------------------")
     input()
-game()
+    os.system('cls')
+    print("----------------------------BLACK JAX---------------------------------")
+    time.sleep(1)
+    print("your goal is to get as close as you can to 21 without going over it...")
+    time.sleep(1)
+    print("you can hit (grab another card) or stand (let the dealer play)")
+    print("Dealer always cheques your pot and you pay a premium for betting...")
+    print("--------------------------Press 'ENTER' to continue-------------------")
+    input()
+##drawing a card phase
+def draw():
+    global total
+    total = 0
+    while total < 22:
+        
+        print("\n\n         hit '1' or stand '2'")
+        x = input()
+        if x=="2":
+               
+            if total <21 and total >16:
+                os.system('cls')
+                print("Close enough..")  
+                input()
+            if total < 17:
+                os.system('cls')
+                print("probbably not Close enough..")  
+                input()
+        if x=="1":
+            card = random.randint(1,13)
+            if card == 11:
+                card = "J"
+            if card ==12:
+                card = "Q"
+            if card ==13:
+                card = "K"
+            print("                            ")
+            print("  __________ ")
+            print(f" |         {card}|               ")
+            print(" |          |               ")
+            print(" |          |               ")
+            print(f" |    {card}     |               ")
+            print(" |          |               ")
+            print(f" |{card}         |               ")
+            print(" |__________|                         ")
+            if card == "J" or card == "Q" or card == "K":
+                    card = 10
+            total = total + card
+            print(f"\n\n         Total:{total}")
+            if total == 21:
+                os.system('cls')
+                print("black jax!!")
+                input()
+    else:
+        os.system('cls')
+        print("Bust")
+        input()
+        exit
+
+#title()
+#shuffle()
+draw()
+
+if total > 21:
+     os.system('cls')
+     print("prolly a loss")
